8f691b8276fa5ead7283a97fb5c1903f5a083b22
Tom Doel <t.doel@ucl.ac.uk>
2024-10-10 16:19:57 +0100

    Add utility function

---
+++ src/drc_containers/xnat_utils/command_line.py
@@ -0,0 +1,15 @@
+import re
+
+
+def string_to_list(arg: str) -> list[str]:
+    """Convert a delimited string into list of strings. The delimiter is any
+    combination of whitespace characters, commas or semicolons. Empty strings
+    are removed from the list
+
+    Args:
+        arg: delimited string
+
+    Returns:
+        list of strings extracted from the input string
+    """
+    return [a for a in re.split(r'[;,\s]+', arg) if a]
